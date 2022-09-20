775f9c618709e7248bbc2e71ec7946ec8ede5023
Yuval Shapira <uv1202@gmail.com>
2022-09-20 13:28:31 +0300

    activity types and setting - seperate file

---
+++ client/src/store.js
@@ -0,0 +1,58 @@
+export default {
+  activitiesType: [
+    {
+      resource_type: "movie",
+      type: "Movie",
+      score: false,
+      zoom: false
+    },
+    {
+      resource_type: "quiz",
+      type: "Quiz",
+      score: true,
+      zoom: true
+    },
+    {
+      resource_type: "easy_quiz",
+      type: "Easy Quiz",
+      score: true,
+      zoom: true
+    },
+    {
+      resource_type: "challenge",
+      type: "Challenge",
+      score: true,
+      zoom: true
+    },
+    {
+      resource_type: "make_a_map",
+      type: "Make a Map",
+      score: false,
+      zoom: true
+    },
+    {
+      resource_type: "make_a_movie",
+      type: "Make a Movie",
+      score: false,
+      zoom: true
+    },
+    {
+      resource_type: "word_play",
+      type: "Word play",
+      score: false,
+      zoom: true
+    },
+    {
+      resource_type: "related_reading",
+      type: "Related reading",
+      score: false,
+      zoom: false
+    },
+    {
+      resource_type: "draw_about_it",
+      type: "Draw about it",
+      score: false,
+      zoom: true
+    }
+  ]
+};
